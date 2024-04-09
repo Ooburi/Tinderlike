d06f980fb0a3193d74a14ac1bdcd237ed87f9c6c
Ooburi <paandu.da@gmail.com>
2024-04-09 22:11:01 +0900

    api + dataLayer; postgress, secrets

---
+++ API/Program.cs
@@ -0,0 +1,39 @@
+using DataLayer.Interfaces;
+using DataLayer.Services;
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+var connection = config["connectionstring"];
+
+builder.Services.AddDbContext<ProfileContext>(options => options.UseNpgsql(connection));
+builder.Services.AddScoped<IProfileService, ProfileService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .SetIsOriginAllowed((host) => true)
+            .AllowAnyHeader());
+});
+// Add services to the container.
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors("CorsPolicy");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- README.md
+++ README.md
@@ -12,3 +12,11 @@
 * C#
 * EntityFrameworkCore
 * PostgreSQL
+
+
+### Before launch add secrets:
+cd API
+dotnet user-secrets init
+dotnet user-secrets set apikey {some api key}
+dotnet user-secrets set connectionstring {connection string}
+cd ..
